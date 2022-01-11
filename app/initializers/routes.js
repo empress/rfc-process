7be6229ad480d6ace75562e1d46b2616c28ea906
Chris Manson <chris@manson.ie>
2022-01-11 10:58:39 +0000

    add id to the rfc route path

--- app/initializers/routes.js
+++ app/initializers/routes.js
@@ -2,7 +2,7 @@ import Router from '../router';
 
 export function initialize() {
   Router.map(function() {
-    this.route('rfc', { path: '/:id' });
+    this.route('rfc', { path: 'id/:id' });
   });
 }
 
