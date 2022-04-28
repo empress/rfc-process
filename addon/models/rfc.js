19cc1b41fe5992b0eb83c82a9036a05f85ba0b21
Chris Manson <chris@manson.ie>
2022-04-28 13:08:41 +0100

    add extra attributes to the rfc model

--- addon/models/rfc.js
+++ addon/models/rfc.js
@@ -4,4 +4,14 @@ const { Model } = DS;
 export default Model.extend({
   content: DS.attr(),
   html: DS.attr(),
+
+  startDate: DS.attr('date'),
+  releaseDate: DS.attr('date'),
+  releaseVersions: DS.attr(),
+  proposalPr: DS.attr(),
+  trackingLink: DS.attr(),
+
+
+  teams: DS.hasMany('team'),
+  stage: DS.belongsTo('stage'),
 });
