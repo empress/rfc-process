f7423eb3c43dff1a4b34f5d6ce46ee1ee7cad814
Chris Manson <chris@manson.ie>
2022-05-07 13:57:21 +0100

    stop using implicit store injection

--- addon/routes/application.js
+++ addon/routes/application.js
@@ -1,5 +1,8 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
 export default class ApplicationRoute extends Route {
+  @service store;
 
   async model() {
     const [, , toc] = await Promise.all([
--- addon/routes/rfc.js
+++ addon/routes/rfc.js
@@ -1,5 +1,8 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
 export default class RfcsRoute extends Route {
+  @service store;
 
   model(params) {
     return this.store.findRecord('rfc', params.id);
